e377c539e88ab07fe086eff73a5f0a219ce8206b
Imp3rial <boo2atfoo@gmail.com>
2013-12-02 19:36:12 -0700

    Added more specific error messages for the types of errors that might pop up.  Possibly implement more functionality for those messages.

--- CustomExceptionList.py
+++ CustomExceptionList.py
@@ -4,20 +4,30 @@
 @date: 01 Dec 13
 @class: CS 359
 '''
+import types
 
 class AbstractBaseException(Exception):
-    def handleItself(self):
-        print("An unknown exception occurred")
-
-class DetailedException(AbstractBaseException):
     
     def __init__(self, msg):
         self.msg = msg
         
     def handleItself(self):
-       if type(self.msg) is string:
-            print("The error that occurred is described by: %s" %(self.msg))
-       elif type(self.msg) is int:
-            print("The error that occurred is described by: %d" %(self.msg))
-       else:
-            print("The error that occurred is described by: %r" %(self.msg))
+        print("An unknown exception occurred: %r" %(self.msg))
+
+class DetailedException(AbstractBaseException):
+        
+    def handleItself(self):
+        if type(self.msg) is types.StringType:
+            print("The error that occurred is described by:\n %s" %(self.msg))
+        elif type(self.msg) is types.IntType:
+            print("The error that occurred is described by:\n %d" %(self.msg))
+        else:
+            print("The error that occurred is described by:\n %r" %(self.msg))
+
+class TooFewPiecesException(AbstractBaseException):
+    def handleItself(self):
+        print("Too few pieces exception hit")
+    
+class TooFewCardsException(AbstractBaseException):
+    def handleItself(self):
+        print("Too few cards exception hit")
