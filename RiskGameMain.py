8f75028f84ebfa5342875f5dc92b6d0450a1cb0f
Imp3rial <boo2atfoo@gmail.com>
2013-12-09 23:36:36 -0700

    Added in the code to make sure that the current player always has a possible move before getting an action.  Set up the board with the five regions randomly being distributed between the two possible players.

--- RiskGameMain.py
+++ RiskGameMain.py
@@ -9,16 +9,44 @@ import CustomExceptionList as CEL
 import RiskGUI
 import RiskBoard
 import pygame
+import Player
+import random
 if __name__ == '__main__':
-    try:
-        GameScreen = RiskGUI.RiskGUI()
-    except CEL.DetailedException as EO:
-        EO.handleItself()
-    board = RiskBoard.RiskBoard()
+    GameScreen = RiskGUI.RiskGUI()  
+    board = RiskBoard.RiskBoard()  
+        # Setup two players for testing
+    players = []
+    one = Player.Player((255, 0, 0), 10, "Player 1")
+    two = Player.Player((0, 0, 255), 10, "Player 2")
+    players.append(one)
+    players.append(two)
+    order = 0
+    for region in board.getRegions():
+        test = random.randint(1,2)
+        if(test == 1):
+            region[1].setPlayer(players[0])
+        else:
+            region[1].setPlayer(players[1])
+        region[1].addUnits(2)
+    #
+
     GameScreen.drawBoard(board)
-    pygame.display.flip()
+    pygame.display.flip()  # update the screen
+    
+
+    
+    
     while True:
+        # Ensure that the current player is capable of making a move
+        count = 0
+        while(board.hasValidMove(players[order]) != True):
+            order = (order + 1) % (len(players))
+            count = count + 1
+            if(count == len(players)):
+                exit(1) # None of the players has a valid move
+                          
         event = GameScreen.getEvent(board)
+        
         print event
         if event == "Exit":
             break
