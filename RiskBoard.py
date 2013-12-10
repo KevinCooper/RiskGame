0e0a8276b10a8fef4f8fe907df04f3727195e5ae
Imp3rial <boo2atfoo@gmail.com>
2013-12-09 23:37:48 -0700

    Added a method that will determine if the player has a possible move.  This is determined by whether they control a region that has more than a single unit on it, since you will not be able to make a move if you only have one.  Can't give up region at this point in the games rules.

--- RiskBoard.py
+++ RiskBoard.py
@@ -13,23 +13,24 @@ class RiskBoard:
         regionPicture = pygame.image.load("circle.png").convert()
         self.graph = nx.Graph()
         self.regions = {}
-        self.regions["Center"] = Region.Region(image=regionPicture, player=None, units=0, name="Center",  position=(300, 300))  
-        self.regions["Left"] =Region.Region(image=regionPicture, player=None, units=0, name="Left", position=(50, 300))
-        self.regions["Top"] =Region.Region(image=regionPicture, player=None, units=0, name="Top",  position=(300, 20))
-        self.regions["Right"] =Region.Region(image=regionPicture, player=None, units=0, name="Right",  position=(500, 300))
-        self.regions["Bottom"] =Region.Region(image=regionPicture, player=None, units=0, name="Bottom", position=(300, 450))
+        self.regions["Center"] = Region.Region(image=regionPicture, player=None, units=0, name="Center", position=(300, 300))  
+        self.regions["Left"] = Region.Region(image=regionPicture, player=None, units=0, name="Left", position=(50, 300))
+        self.regions["Top"] = Region.Region(image=regionPicture, player=None, units=0, name="Top", position=(300, 20))
+        self.regions["Right"] = Region.Region(image=regionPicture, player=None, units=0, name="Right", position=(500, 300))
+        self.regions["Bottom"] = Region.Region(image=regionPicture, player=None, units=0, name="Bottom", position=(300, 450))
         
-        self.graph.add_node(self.regions["Center"])# Center Region
-        self.graph.add_node(self.regions["Left"])# Left Region
-        self.graph.add_node(self.regions["Top"])# Top Region
-        self.graph.add_node(self.regions["Right"])# Right Region
-        self.graph.add_node(self.regions["Bottom"])# Bottom Region
+        self.graph.add_node(self.regions["Center"])  # Center Region
+        self.graph.add_node(self.regions["Left"])  # Left Region
+        self.graph.add_node(self.regions["Top"])  # Top Region
+        self.graph.add_node(self.regions["Right"])  # Right Region
+        self.graph.add_node(self.regions["Bottom"])  # Bottom Region
+        
         self.graph.add_edge(self.regions["Center"], self.regions["Left"])
         self.graph.add_edge(self.regions["Center"], self.regions["Top"])
         self.graph.add_edge(self.regions["Center"], self.regions["Right"])
         self.graph.add_edge(self.regions["Center"], self.regions["Bottom"])
-        for neighbor in self.graph.neighbors(self.regions["Center"]):
-            print neighbor
+        # for neighbor in self.graph.neighbors(self.regions["Center"]):
+        #   print neighbor
             
     def getRegions(self):
         if self.regions != None:
@@ -39,4 +40,10 @@ class RiskBoard:
         
     def getNeighbors(self, region):
         return self.graph.neighbors(self.regions[region])
+    
+    def hasValidMove(self, player):
+        for region in self.regions:
+            if(self.regions[region].getPlayer() == player and self.regions[region].canMove()):
+                return True
+        return False
         
