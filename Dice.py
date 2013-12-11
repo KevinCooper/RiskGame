47fc4d48b6de7fe6c684b8eb0c272d7518b36cc4
Imp3rial <boo2atfoo@gmail.com>
2013-12-11 01:09:59 -0700

    Finished commenting the Dice class.

--- Dice.py
+++ Dice.py
@@ -3,18 +3,32 @@
 @version: 0.0.1
 @date: 01 Dec 13
 @class: CS 359
-@purpose: This file contains the class for a simulated dice.  When used, it \
-returns a random number between 1 and the value chose when the dice was first\
- initialized
+@purpose: This file contains the class for a simulated dice.  
 '''
 import random
 import os
 
 
 class Dice:
+    '''
+    When used, it returns a random number between 1 and the\
+    value chosen when the dice was first initialized
+    '''
     def __init__(self, sides):
+        '''
+        sides - integer
+
+        Takes an integer number of simulated sides that the dice\
+        should have.
+        '''
         self._sides = sides
         random.seed(os.urandom(16))
 
     def rollDice(self):
+        '''
+        return - integer
+
+        Returns a random integer number between 1 and sides.  The sequence of \
+        numbers will be different for each dice used
+        '''
         return random.randint(1, self._sides)
