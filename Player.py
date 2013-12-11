d8fe26481c24bba420adccb9f65948100ad5856d
Imp3rial <boo2atfoo@gmail.com>
2013-12-11 01:06:12 -0700

    Finished commenting and formatting the player file.

--- Player.py
+++ Player.py
@@ -8,29 +8,49 @@ import CustomExceptionList as CEL
 
 
 class Player:
+    '''
+    Class that contains information about a player
+    '''
     def __init__(self, color, pieces, name):
+        '''
+        color - 3 integer tuple
+        pieces - integer number of starting pieces
+        name - string player name
+        '''
         self._color = color
         self._pieces = pieces
         self._name = name
         self._cards = []
 
     def __str__(self):
+        '''
+        return - String player name
+        '''
         return self._name
 
     def getPieces(self):
+        '''
+        return - integer number of pieces the player has
+        '''
         return self._pieces
 
     def getColor(self):
+        '''
+        return - 3 integer tuple representing the RGB color values
+        '''
         return self._color
 
     def addCard(self, card):
+        ''' Not implemented'''
         self._cards.append(card)
 
     def getCards(self):
+        ''' Not implemented'''
         for item in self._cards:
             yield self._cards[item]
 
     def removeCard(self, card):
+        ''' Not implemented'''
         try:
             if(self._cards == []):
                 raise CEL.TooFewCardsException
@@ -39,9 +59,19 @@ class Player:
             error.handleItself()
 
     def addPieces(self, numberToAdd):
+        '''
+        numberToAdd - integer
+
+        Adds the given number to the total number of pieces the player has.
+        '''
         self._pieces += numberToAdd
 
     def removePieces(self, numberToLose):
+        '''
+        numberToLose - integer
+
+        Subtracts the given number to the total number of pieces the player has.
+        '''
         if(self._pieces - numberToLose < 0):
             return False
         self._pieces -= numberToLose
